c191424eac4268f788b51ec9c8a89316d36133d6
Ondřej Míchal <harrymichal@seznam.cz>
2025-10-09 00:16:44 +0300

    misc: Stop keeping track of the historical values of metrics

--- src/opencep/misc/StudentMetrics.py
+++ src/opencep/misc/StudentMetrics.py
@@ -9,11 +9,6 @@ class Metrics(StrEnum):
     PROCESSED_EVENTS = "processed_events"
 
 
-_metrics = {
-    Metrics.EVENT_PROCESSING_LATENCY: [],
-    Metrics.PROCESSED_EVENTS: 0,
-}
-
 lock = Lock()
 
 
@@ -26,13 +21,11 @@ def _log_metric(msg: str):
 def increment_counter(metric: Metrics, cur_time: int = 0):
     assert isinstance(metric, Metrics)
     cur_time = time.perf_counter_ns if cur_time == 0 else cur_time
-    _metrics[metric] += 1
     _log_metric(f"{cur_time} counter {str(metric)} 1")
 
 
 def mark_hist_point(metric: Metrics, value, attrs: dict[str, Any], cur_time: int = 0):
     assert isinstance(metric, Metrics)
     cur_time = time.perf_counter_ns if cur_time == 0 else cur_time
-    _metrics[metric] += (value, attrs)
     attrs_str = " ".join([f"{key} {val}" for key, val in attrs.items()])
     _log_metric(f"{cur_time} hist {str(metric)} {value} {attrs_str}")
